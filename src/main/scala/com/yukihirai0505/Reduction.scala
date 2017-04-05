21b4860ce9d9f985e6b30f31ea83acb0569b58d2
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-05 19:59:41 +0900

    Update Reduction Object

--- src/main/scala/com/yukihirai0505/Reduction.scala
+++ src/main/scala/com/yukihirai0505/Reduction.scala
@@ -8,17 +8,11 @@ object Reduction {
   var firstFormula = Array.empty[String]
   for (line <- stdin.getLines) {
     // ユークリッドの互除法
-    def euclideanAlgorithm(r: Int, a: Int, b: Int): Int = {
+    def euclideanAlgorithm(r: Int, b: Int): Int = {
       if (r == 0) {
         b
       } else {
-        var _a = a
-        var _b = b
-        var _r = r
-        _a = _b
-        _b = _r
-        _r = _a % _b
-        euclideanAlgorithm(_r, _a, _b)
+        euclideanAlgorithm(b%r, r)
       }
     }
 
@@ -31,21 +25,14 @@ object Reduction {
       val secondMolecule = secondFormula(0).toInt
       val secondDenominator = secondFormula(1).toInt
       val x = firstDenominator * secondDenominator
-      val r1 = if (firstDenominator<secondDenominator) secondDenominator % firstDenominator else firstDenominator % secondDenominator
       // 最大公約数
-      val greatestCommonFactor1 = euclideanAlgorithm(r1, firstDenominator, secondDenominator)
+      val greatestCommonFactor1 = euclideanAlgorithm(firstDenominator, secondDenominator)
       // 最小公倍数
-      val leastCommonMultiple = (greatestCommonFactor1!=firstDenominator, greatestCommonFactor1!=secondDenominator) match {
-        case (true, true) => x/greatestCommonFactor1
-        case (false, true) => firstDenominator
-        case (true, false) => secondDenominator
-        case _ => secondDenominator
-      }
+      val leastCommonMultiple = x/greatestCommonFactor1
       // 最小公倍数からそれぞれの式に掛け算して分子を合計する
       val totalMolecule = ((leastCommonMultiple/firstDenominator) * firstMolecule) + ((leastCommonMultiple/secondDenominator) * secondMolecule)
-      val r2 = if (leastCommonMultiple<totalMolecule) totalMolecule % leastCommonMultiple else leastCommonMultiple % totalMolecule
       // 最大公約数を算出して約分したデータを出力
-      val greatestCommonFactor2 = euclideanAlgorithm(r2, leastCommonMultiple, totalMolecule)
+      val greatestCommonFactor2 = euclideanAlgorithm(leastCommonMultiple, totalMolecule)
       val resultMolecule = totalMolecule/greatestCommonFactor2
       val resultDenominator = leastCommonMultiple/greatestCommonFactor2
       if (resultMolecule == resultDenominator) {
