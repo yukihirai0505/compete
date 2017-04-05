5e58044443fda3838e5e652ad82abd8cfdb35755
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-05 20:21:59 +0900

    Add FizzBuzz

---
+++ src/main/scala/com/yukihirai0505/FizzBuzz.scala
@@ -0,0 +1,13 @@
+package com.yukihirai0505
+
+/***
+  * FizzBuzz
+  */
+object FizzBuzz extends App {
+  (1 to 100).map(i => (i % 3, i % 5) match {
+    case (0, 0) => "FizzBuzz"
+    case (0, _) => "Fizz"
+    case (_, 0) => "Buzz"
+    case _ => i
+  }).foreach(println)
+}
