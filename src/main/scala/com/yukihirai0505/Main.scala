b09b6348ac4c037428921c9e1f2859d3eba2f1e8
Yuki Hirai <yukihirai0505@gmail.com>
2017-04-07 19:24:52 +0900

    WIP: Add FrequencyDistribution Object

--- src/main/scala/com/yukihirai0505/Main.scala
+++ src/main/scala/com/yukihirai0505/Main.scala
@@ -1,45 +1,39 @@
 package com.yukihirai0505
 
-import scala.io.Source.stdin
-
-object TrianglePattern {
+object Main extends App {
   import scala.io.Source.stdin
-  var count: Long = 0
-  var sumOdd: Long = 0
-  var sumEven: Long = 0
+  import scala.math.ceil
+  var result = ""
+  def isPrimeNum(i: Int): Boolean = {
+    if (i <= 1)
+      false
+    else if (i == 2)
+      true
+    else
+      !(2 until i).exists(x => i % x == 0)
+  }
+  def displayNum(i: Int): String = {
+    if (i < 10) {s"0$i"} else i.toString
+  }
   for (line <- stdin.getLines) {
-    val input = line.toInt
-    for (i <- 1 to input) {
-      // 偶数の時
-      if ((i&1) == 0) {
-        sumOdd+=i
-        count+=sumOdd
-      } else {
-        sumEven+=i
-        count+=sumEven
+    val input = line.split(" ").map(_.toInt)
+    val total = input(0)
+    val repeatTimes = input(1)
+    for (i <- 1 to ceil(total / repeatTimes).toInt + 1) {
+      val from = (i - 1) * repeatTimes + 1
+      val to = from - 1 + repeatTimes
+      if (from < total) {
+        val asterisks = if (to > total) {
+          (from to total).filter(isPrimeNum).map(_ => "*").mkString("")
+        } else (from to to).filter(isPrimeNum).map(_ => "*").mkString("")
+        val totalDigits = total.toString.length
+        val fromDigits = displayNum(from).toString.length
+        val toDisits = displayNum(to).toString.length
+        val diffDisits = (1 until totalDigits - fromDigits + totalDigits - toDisits).map(_ => " ").mkString("")
+        result += s"${displayNum(from)}-${displayNum(to)}$diffDisits:$asterisks\n"
       }
     }
-    println(count)
-    count=0
-    sumOdd = 0
-    sumEven = 0
-  }
-}
-
-object TrianglePatternSlow {
-  def isTriangle(a: Int, b: Int, c: Int): Boolean = {
-    a + b >= c && b + c >= a && c + a >= b
-  }
-  for (line <- stdin.getLines) {
-    val input = line.toInt
-    // 全ての組み合わせ
-    val list = 1 to input
-    // 1つ選ぶ時の組み合わせはinput分ある
-    val combi1 = input
-    // 2つ選ぶ時の組み合わせ
-    val combi2 = list.combinations(2).count(x => isTriangle(x.head, x.last, x.last) || isTriangle(x.head, x.head, x.last))
-    // 3つ選ぶ時の組み合わせ
-    val combi3 = list.combinations(3).count(x => isTriangle(x.head, x.tail.head, x.last))
-    println(combi1+combi2+combi3)
+    println(result.patch(result.lastIndexOf("\n"), "", 1))
+    result = ""
   }
 }
