621314ccd40d0aab9914c26c4ae3640da43eaa42
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-12 12:12:06 +0900

    Refactor SpeedGame

--- core/src/main/scala/com/yukihirai0505/SpeedGame.scala
+++ core/src/main/scala/com/yukihirai0505/SpeedGame.scala
@@ -1,26 +1,17 @@
 package com.yukihirai0505
 
-object SpeedGame {
-  import scala.io.Source.stdin
-  var firstGame = Seq.empty[Int]
-  var secondGame = Seq.empty[Int]
-  var firstResult = Seq.empty[Int]
-  var firstWinner = Seq.empty[Int]
-  for (line <- stdin.getLines) {
-    (firstGame, secondGame, firstResult) match {
-      case (f, _, _) if f.isEmpty => firstGame = line.split(" ").map(_.toInt)
-      case (f, s, _) if f.nonEmpty && s.isEmpty => secondGame = line.split(" ").map(_.toInt)
-      case (f, s, r) if f.nonEmpty && s.nonEmpty && r.isEmpty =>
-        firstResult = line.split(" ").map(_.toInt)
-        if (firstResult(firstGame.head - 1) < firstResult(firstGame.last - 1)) firstWinner = Seq(firstGame.head)
-        else firstWinner = Seq(firstGame.last)
-        if (firstResult(secondGame.head - 1) < firstResult(secondGame.last - 1)) firstWinner = firstWinner :+ secondGame.head
-        else firstWinner = firstWinner :+ secondGame.last
-      case (f, s, r) if f.nonEmpty && s.nonEmpty && r.nonEmpty =>
-        val secondResult = line.split(" ").map(_.toInt)
-        val sortedFirstWinner = firstWinner.sortBy(x => x)
-        if (secondResult.head < secondResult.last) println(s"${sortedFirstWinner.head}\n${sortedFirstWinner.last}")
-        else println(s"${sortedFirstWinner.last}\n${sortedFirstWinner.head}")
-    }
-  }
+object SpeedGame extends App {
+  val sc = new java.util.Scanner(System.in)
+  val firstGame = sc.nextLine().split(" ").map(_.toInt)
+  val secondGame = sc.nextLine().split(" ").map(_.toInt)
+  val firstResult = sc.nextLine().split(" ").map(_.toInt)
+  val secondResult = sc.nextLine().split(" ").map(_.toInt)
+  val firstWinner = (if (firstResult(firstGame.head - 1) < firstResult(firstGame.last - 1)) firstGame.head
+  else firstGame.last,
+  if (firstResult(secondGame.head - 1) < firstResult(secondGame.last - 1)) secondGame.head
+  else secondGame.last)
+  val sortedFirstWinner = Seq(firstWinner._1, firstWinner._2).sorted
+  if (secondResult.head < secondResult.last)
+    println(s"${sortedFirstWinner.head}\n${sortedFirstWinner.last}")
+  else println(s"${sortedFirstWinner.last}\n${sortedFirstWinner.head}")
 }
