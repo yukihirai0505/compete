4e44d29a187c45269258d6c84bf654c9e7d2cb5d
Yuki Hirai <yukihirai0505@gmail.com>
2017-05-12 14:35:25 +0900

    Refactor Combination

--- core/src/main/scala/com/yukihirai0505/Combination.scala
+++ core/src/main/scala/com/yukihirai0505/Combination.scala
@@ -1,19 +1,13 @@
 package com.yukihirai0505
 
-/***
+/** *
   * 指定された配列のなかから2つの組み合わせでその和が256になるものがあるかどうか
   */
-object Combination {
-  import scala.io.Source.stdin
-  var arrayNum  = 0
-  for (line <- stdin.getLines) {
-    if (arrayNum.equals(0)) {
-      arrayNum = line.toInt
-    } else {
-      val nums = line.split(" ").map(_.toInt)
-      // nCrをcombinationsメソッドで計算 n個からr個選ぶ組み合わせ
-      val combinations = nums.combinations(2)
-      println(if (combinations.exists(combi => combi.sum == 256)) "yes" else "no")
-    }
-  }
+object Combination extends App {
+  val sc = new java.util.Scanner(System.in)
+  val arrayNum = sc.nextLine().toInt
+  val nums = sc.nextLine().split(" ").map(_.toInt)
+  // nCrをcombinationsメソッドで計算 n個からr個選ぶ組み合わせ
+  val combinations = nums.combinations(2)
+  println(if (combinations.exists(combi => combi.sum == 256)) "yes" else "no")
 }
