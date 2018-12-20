46750955ec0c0e0a2eec07675b49e7c24c87ffd2
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-20 13:41:47 +0900

    Add GetTotalX

---
+++ core/src/main/scala/com/jobinterview/GetTotalX.scala
@@ -0,0 +1,19 @@
+package com.jobinterview
+
+object GetTotalX {
+
+  def getTotalX(a: Array[Int], b: Array[Int]): Int = {
+    // 4 to 16
+    (a.max to b.min).count(i => {
+      // 2, 4 % 3 (4 to 16) && 16, 32, 96 % (4 to 16)
+      a.forall(i % _ == 0) && b.forall(_ % i == 0)
+    })
+  }
+
+  def main(args: Array[String]) {
+
+    val a = Array(2, 4)
+    val b = Array(16, 32, 96)
+    println(getTotalX(a, b))
+  }
+}
