e9608d22927ffea8e342a922a16a010615289063
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-28 00:32:43 +0900

    Update LargestPrimeFactor

--- core/src/main/scala/com/jobinterview/LargestPrimeFactor.scala
+++ core/src/main/scala/com/jobinterview/LargestPrimeFactor.scala
@@ -1,19 +1,40 @@
 package com.jobinterview
 
-
+/*
+100=>5
+600851475143=>6857
+1000000000001=>99990001
+*/
 object LargestPrimeFactor {
-  def solution(n: Long): Long = {
-    val maxFactor = Math.sqrt(n)
-    @scala.annotation.tailrec
-    def find(x: Long, factor: Long): Long = {
-      if (x % factor == 0) {
-        factor match {
-          case d if d > maxFactor || x == d => d
-          case d => find(x / d, d)
+
+  def loser(n: Long): Long = {
+    val maxFactor = Math.sqrt(n).toInt + 1
+    var max = 0L
+    var a = n
+    for (j <- 2 to maxFactor) {
+      while (a % j == 0) {
+        if (j > max) {
+          max = j
         }
-      } else find(x, factor + 1)
+        a /= j
+      }
     }
-    find(n, 2)
+    if (a > 1 && a > max)
+      max = a
+    max
+  }
+
+  def solution(n: Long): Long = {
+    val maxFactor = Math.sqrt(n).toInt + 1
+
+    def findMax(x: Long, factor: Long = 2, max: Long = 0): Long = {
+      if (factor <= maxFactor)
+        if (x % factor == 0) findMax(x / factor, factor, factor)
+        else findMax(x, factor + 1, max)
+      else if (x > 1 && x > max) x else max
+    }
+
+    findMax(n)
   }
 
   def main(args: Array[String]) {
