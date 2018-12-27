95da0258f3b37030d58ed6e26f3ae5c59b5733b6
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-27 22:19:52 +0900

    Update Largestprimefactor

--- core/src/main/scala/com/jobinterview/LargestPrimeFactor.scala
+++ core/src/main/scala/com/jobinterview/LargestPrimeFactor.scala
@@ -1,25 +1,18 @@
 package com.jobinterview
 
+
 object LargestPrimeFactor {
-
-  def isPrime(value: Long): Boolean = {
-    !(2L to math.sqrt(value).toInt).exists(value % _ == 0)
-  }
-
   def solution(n: Long): Long = {
-    if (isPrime(n)) return n
-
     val maxFactor = Math.sqrt(n)
-
-    def find(x: Long, dividend: Long): Long = {
-      if (x % dividend == 0 && isPrime(dividend)) {
-        dividend match {
+    @scala.annotation.tailrec
+    def find(x: Long, factor: Long): Long = {
+      if (x % factor == 0) {
+        factor match {
           case d if d > maxFactor || x == d => d
           case d => find(x / d, d)
         }
-      } else find(x, dividend + 1)
+      } else find(x, factor + 1)
     }
-
     find(n, 2)
   }
 
