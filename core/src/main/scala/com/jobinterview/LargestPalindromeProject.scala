fe022093ef32f007166aff8dad89cea69c442a2d
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-29 08:25:05 +0900

    Add LargestPalindromeProject

--- core/src/main/scala/com/jobinterview/LargestPalindromeProject.scala
+++ core/src/main/scala/com/jobinterview/LargestPalindromeProject.scala
@@ -1,14 +1,26 @@
 package com.jobinterview
 
+/**
+  * 2
+  * 101110 => 101101 (143 * 707)
+  * 800000 => 793397 (869 * 913)
+  */
 object LargestPalindromeProject {
 
-  def solution(n: Long): Long = {
-    // TODO
+  def isPalindrome(str: String): Boolean = str == str.reverse
+
+  def solution(n: Int, values: Seq[Int]): Int = {
+    values.find(v => v < n).head
   }
 
   def main(args: Array[String]) {
+    val values = (for {i <- 100 to 999
+                       j <- 100 to 999
+    } yield i * j).filter(product => isPalindrome(product.toString)).sortBy(-_)
     val sc = new java.util.Scanner(System.in)
     val times = sc.nextInt()
-    (0 until times).foreach(_ => println(solution(sc.nextLong())))
+    (0 until times).foreach(_ => println(
+      solution(sc.nextInt(), values))
+    )
   }
 }
