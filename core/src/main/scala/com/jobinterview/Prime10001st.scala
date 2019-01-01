b919492b175daf22b3a47bfa5cdfddca51e5b741
Yuki Hirai <yukihirai0505@gmail.com>
2019-01-01 13:47:45 +0900

    Add Prime10001st

---
+++ core/src/main/scala/com/jobinterview/Prime10001st.scala
@@ -0,0 +1,15 @@
+package com.jobinterview
+
+object Prime10001st {
+
+  def isPrime(num: Int): Boolean =
+    (num > 1) && !(2 to scala.math.sqrt(num).toInt).exists(x => num % x == 0)
+
+  def main(args: Array[String]) {
+    // 10001st prime is 104743
+    val primes: Seq[Int] = (1 to 104743).filter(isPrime)
+    val sc = new java.util.Scanner(System.in)
+    val times = sc.nextInt()
+    (0 until times).foreach(_ => println(primes(sc.nextInt() - 1)))
+  }
+}
